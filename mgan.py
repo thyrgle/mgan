f4dd5340339800fc8db313f1c9070c9ac9afda89
Christopher Sumnicht <csumnicht@berkeley.edu>
2018-02-05 21:50:49 -0800

    Some basic skeleton

---
+++ mgan.py
@@ -0,0 +1,20 @@
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Discriminator(nn.Module):
+
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+        pass
+
+class Generator(nn.Module):
+
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+        pass
