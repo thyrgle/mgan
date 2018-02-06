d90cb5f1598a367af3b17519b20a115a1d8ef443
Christopher Sumnicht <csumnicht@berkeley.edu>
2018-02-05 22:21:32 -0800

    Tentative discriminator

--- mgan.py
+++ mgan.py
@@ -8,8 +8,24 @@ class Discriminator(nn.Module):
     def __init__(self):
         super(Discriminator, self).__init__()
 
+        # Almost the same as the one in the tutorial
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 1)
+
     def forward(self, x):
-        pass
+        # Almost the same as the one in the tutorial
+        x = F.max_pool2d(F.relu(self.conv1(x)), 2)
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = x.view(-1, self._num_flat_features(x))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
 
 class Generator(nn.Module):
 
@@ -31,4 +47,4 @@ class Generator(nn.Module):
 
 
 if __name__ == "__main__":
-    generator = Generator()
+    discriminator = Discriminator()
