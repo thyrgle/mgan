aed7b978eb5525063092bc2ab598a08cf1d1baae
Christopher Sumnicht <csumnicht@berkeley.edu>
2018-02-27 19:00:54 -0800

    Fixed some errors with dimensions

--- mgan.py
+++ mgan.py
@@ -36,18 +36,26 @@ class Discriminator(nn.Module):
         super(Discriminator, self).__init__()
 
         # Almost the same as the one in the tutorial
-        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.conv1 = nn.Conv2d(4, 6, 5)
         self.conv2 = nn.Conv2d(6, 16, 5)
 
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc1 = nn.Linear(82944, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 1)
+
+    def _num_flat_features(self, x):
+        size = x.size()[1:]  # all dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
 
     def forward(self, x):
         # Almost the same as the one in the tutorial
         x = F.max_pool2d(F.relu(self.conv1(x)), 2)
         x = F.max_pool2d(F.relu(self.conv2(x)), 2)
         x = x.view(-1, self._num_flat_features(x))
+        print(x)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
@@ -75,11 +83,12 @@ class Generator(nn.Module):
 def train(epochs, d_steps, g_steps):
     pokemon_dataset = Pokemon()
     pokemon_loader = torch.utils.data.DataLoader(pokemon_dataset,
-                                                 batch_size=d_steps,
+                                                 batch_size=1,
                                                  shuffle=True,
                                                  num_workers=4)
     discriminator = Discriminator()
     generator = Generator()
+    optimizer = torch.optim.Adam(discriminator.parameters())
     for epoch in range(epochs):
         for (inputs, targets) in pokemon_loader:
             # Train the discriminator.
@@ -87,6 +96,10 @@ def train(epochs, d_steps, g_steps):
             targets = Variable(targets)
 
             result = discriminator(inputs)
+            loss = F.mse_loss(result, targets)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
         for g_index in range(d_steps):
             # Train the generator.
             pass
